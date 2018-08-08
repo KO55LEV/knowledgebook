9f05829e1207bf29c4b76aa485562824f081f764
ko55lev <drey2005+KO55LEV@gmail.com>
2018-08-08 20:26:31 +0100

    Threading example updated

--- Multithreading/ExampleAsyncMultithreading.cs
+++ Multithreading/ExampleAsyncMultithreading.cs
@@ -30,3 +30,19 @@ namespace ConsoleApp1
     }
 
 }
+
+
+/*******************************************
+
+And the output is:
+
+Starting Long Running method...
+Press any key to exit...
+End Long Running method...
+Thus,
+
+Main starts the long running method via TestAsyncAwaitMethods. That immediately returns without halting the current thread and we immediately see 'Press any key to exit' message
+All this while, the LongRunningMethod is running in the background. Once its completed, another thread from Threadpool picks up this context and displays the final message
+Thus, not thread is blocked
+
+******************************************/
