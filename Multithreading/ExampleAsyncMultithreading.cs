8bfcbd0e85520c0d5e71282482ae1439b9844639
ko55lev <drey2005+KO55LEV@gmail.com>
2018-08-08 20:23:56 +0100

    Add example of Threading

---
+++ Multithreading/ExampleAsyncMultithreading.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestAsyncAwaitMethods();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        public async static void TestAsyncAwaitMethods()
+        {
+            await LongRunningMethod();
+        }
+
+        public static async Task<int> LongRunningMethod()
+        {
+            Console.WriteLine("Starting Long Running method...");
+            await Task.Delay(5000);
+            Console.WriteLine("End Long Running method...");
+            return 1;
+        }
+    }
+
+}
